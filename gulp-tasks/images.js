40b6d60ecf79a43485829559c4a6b2cb09148a9a
Duncan Geere <radio.edit@gmail.com>
2024-06-13 10:48:59 +0200

    Remove minification

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -9,21 +9,21 @@ const images = () => {
   // We have specific configs for jpeg and png files to try
   // to really pull down asset sizes
   return src('./src/images/**/*')
-    .pipe(
-      imagemin(
-        [
-          mozjpeg({quality: 60, progressive: true}),
-          optipng({optimizationLevel: 5})
-        ],
-        {
-          silent: true
-        }
-      )
-    )
-    .on('error', function (err) { 
-      console.error('Image minification error:', err.message);
-      this.emit('end');
-    })
+    //.pipe(
+    //  imagemin(
+    //    [
+    //      mozjpeg({quality: 60, progressive: true}),
+    //      optipng({optimizationLevel: 5})
+    //    ],
+    //    {
+    //      silent: true
+    //    }
+    //  )
+    //)
+    //.on('error', function (err) { 
+    //  console.error('Image minification error:', err.message);
+    //  this.emit('end');
+    //})
     .pipe(dest('./dist/images'));
 };
 

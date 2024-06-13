64f34db2936d64f7c494150929636b4587a8d292
Duncan Geere <radio.edit@gmail.com>
2024-06-13 11:29:27 +0200

    Another fix attempt

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,8 +1,29 @@
-const { dest, src } = require('gulp');
+const {dest, src} = require('gulp');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const optipng = require('imagemin-optipng');
 
-// Temporarily skip imagemin to test if the issue is with the source image
+// Grabs all images, runs them through imagemin
+// and plops them in the dist folder
 const images = () => {
-  return src('./src/images/**/*')
+  // We have specific configs for jpeg and png files to try
+  // to really pull down asset sizes
+  return src('./src/images/**/*', {encoding: false})
+    .pipe(
+      imagemin(
+        [
+          mozjpeg({quality: 60, progressive: true}),
+          optipng({optimizationLevel: 5, interlaced: null})
+        ],
+        {
+          silent: true
+        }
+      )
+    )
+    .on('error', function (err) { 
+      console.error('Image minification error:', err.message);
+      this.emit('end');
+    })
     .pipe(dest('./dist/images'));
 };
 

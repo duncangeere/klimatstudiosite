ba50eefc839aa9f5ff9e50fa9e64d25420443d85
Duncan Geere <radio.edit@gmail.com>
2024-06-13 10:57:35 +0200

    Removing minification completely

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,29 +1,8 @@
-const {dest, src} = require('gulp');
-const imagemin = require('gulp-imagemin');
-const mozjpeg = require('imagemin-mozjpeg');
-const optipng = require('imagemin-optipng');
+const { dest, src } = require('gulp');
 
-// Grabs all images, runs them through imagemin
-// and plops them in the dist folder
+// Temporarily skip imagemin to test if the issue is with the source image
 const images = () => {
-  // We have specific configs for jpeg and png files to try
-  // to really pull down asset sizes
   return src('./src/images/**/*')
-    //.pipe(
-    //  imagemin(
-    //    [
-    //      mozjpeg({quality: 60, progressive: true}),
-    //      optipng({optimizationLevel: 5})
-    //    ],
-    //    {
-    //      silent: true
-    //    }
-    //  )
-    //)
-    //.on('error', function (err) { 
-    //  console.error('Image minification error:', err.message);
-    //  this.emit('end');
-    //})
     .pipe(dest('./dist/images'));
 };
 

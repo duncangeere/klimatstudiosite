815de10a20f94e984e7c5c87d4ff9f84a689b0cc
kotte <konstantin.aistre@gmail.com>
2024-06-25 16:35:04 +0200

    Fix: Update gulp build to work with ESM so that image optimization will work.

--- gulp-tasks/images.mjs
+++ gulp-tasks/images.mjs
@@ -1,30 +1,30 @@
-const {dest, src} = require('gulp');
-const imagemin = require('gulp-imagemin');
-const mozjpeg = require('imagemin-mozjpeg');
-const optipng = require('imagemin-optipng');
+import { dest, src } from 'gulp';
+import imagemin from 'gulp-imagemin';
+import mozjpeg from 'imagemin-mozjpeg';
+import optipng from 'imagemin-optipng';
 
 // Grabs all images, runs them through imagemin
 // and plops them in the dist folder
 const images = () => {
   // We have specific configs for jpeg and png files to try
   // to really pull down asset sizes
-  return src('./src/images/**/*', {encoding: false})
+  return src('./src/images/**/*', { encoding: false })
     .pipe(
       imagemin(
         [
-          mozjpeg({quality: 60, progressive: true}),
-          optipng({optimizationLevel: 5, interlaced: null})
+          mozjpeg({ quality: 60, progressive: true }),
+          optipng({ optimizationLevel: 5, interlaced: null }),
         ],
         {
-          silent: true
+          silent: true,
         }
       )
     )
-    .on('error', function (err) { 
+    .on('error', function (err) {
       console.error('Image minification error:', err.message);
       this.emit('end');
     })
     .pipe(dest('./dist/images'));
 };
 
-module.exports = images;
+export default images;
